cdca532994c6f872d6ecdd99a2b5e901a5adc273
thomasalbertoDev <thomasalberto456@gmail.com>
2024-06-05 01:07:25 +0700

    Belajar Provider Service

---
+++ src/user/user/user.service.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
+
+describe('UserService', () => {
+  let service: UserService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserService],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+  });
+
+  it('should be able say hello', () => {
+    const response = service.sayHello('Thomas');
+    expect(response).toBe('Hello Thomas!');
+  });
+});
