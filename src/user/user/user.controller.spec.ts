f63a15559476b557c847a6e419371b2fd50c1e6a
thomasalbertoDev <thomasalberto456@gmail.com>
2024-06-05 00:51:33 +0700

    Belajar Unit Test

--- src/user/user/user.controller.spec.ts
+++ src/user/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import * as httpMock from 'node-mocks-http';
 
 describe('UserController', () => {
   let controller: UserController;
@@ -12,7 +13,19 @@ describe('UserController', () => {
     controller = module.get<UserController>(UserController);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should can say hello', async () => {
+    const response = await controller.sayHello('Thomas', 'Alberto');
+    expect(response).toBe('Hello Thomas Alberto');
+  });
+
+  it('should can view template', async () => {
+    const response = httpMock.createResponse();
+    controller.viewHello('Thomas', response);
+
+    expect(response._getRenderView()).toBe('index.html');
+    expect(response._getRenderData()).toEqual({
+      title: 'Belajar View NestJS',
+      name: 'Hello Thomas',
+    });
   });
 });
