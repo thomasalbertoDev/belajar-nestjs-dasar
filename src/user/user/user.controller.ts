be49283227196c76fa4aabb03acb9c3d6dcdbadb
thomasalbertoDev <thomasalberto456@gmail.com>
2024-06-05 00:18:57 +0700

    belajar cookie

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -8,10 +8,24 @@ import {
   Post,
   Query,
   Redirect,
+  Req,
+  Res,
 } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Controller('/api/users')
 export class UserController {
+  @Get('/set-cookie')
+  setCookie(@Query('name') name: string, @Res() response: Response) {
+    response.cookie('name', name);
+    response.status(200).send('Cookie is set');
+  }
+
+  @Get('/get-cookie')
+  getCookie(@Req() request: Request): string {
+    return request.cookies['name'];
+  }
+
   @Get('/sample-response')
   @Header('Content-Type', 'application/json')
   @HttpCode(200)
