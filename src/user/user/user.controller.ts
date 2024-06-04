8e9cfa11c23a279bf6abfb4b2b51df92eb30ca49
thomasalbertoDev <thomasalberto456@gmail.com>
2024-06-04 12:57:36 +0700

    belajar HTTP Method

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('/api/users')
-export class UserController {}
+export class UserController {
+  @Post()
+  post(): string {
+    return 'Post User';
+  }
+
+  @Get('/sample')
+  get(): string {
+    return 'Sample User';
+  }
+}
