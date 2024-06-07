2912107cd47c7456323a5adeb2dba2eb27b7a892
thomasalbertoDev <thomasalberto456@gmail.com>
2024-06-07 19:53:08 +0700

    Belajar Guard

--- src/user/user/user.controller.ts
+++ src/user/user/user.controller.ts
@@ -15,6 +15,7 @@ import {
   Req,
   Res,
   UseFilters,
+  UseGuards,
   UseInterceptors,
   UsePipes,
 } from '@nestjs/common';
@@ -33,6 +34,7 @@ import {
 import { ValidationPipe } from 'src/validation/validation.pipe';
 import { TimeInterceptor } from 'src/time/time.interceptor';
 import { Auth } from 'src/auth/auth.decorator';
+import { RoleGuard } from 'src/role/role.guard';
 
 @Controller('/api/users')
 export class UserController {
@@ -46,6 +48,7 @@ export class UserController {
   ) {}
 
   @Get('/current')
+  @UseGuards(new RoleGuard(['admin', 'operator']))
   current(@Auth() user: User): Record<string, any> {
     return {
       data: `Welcome ${user.first_name} ${user.last_name}`,
